ca470d4827e9e738dc7b81d10a2208995404b649
NadyaCodes <nadyacorscadden@gmail.com>
2022-05-05 13:28:27 -0400

    make addProperty function use pool

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -214,9 +214,45 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+
+  const queryParams = []
+
+  queryParams.push(property.owner_id)
+  queryParams.push(property.title)
+  queryParams.push(property.description)
+  queryParams.push(property.thumbnail_photo_url)
+  queryParams.push(property.cover_photo_url)
+  queryParams.push(property.cost_per_night * 100)
+  queryParams.push(property.street)
+  queryParams.push(property.city)
+  queryParams.push(property.province)
+  queryParams.push(property.post_code)
+  queryParams.push(property.country)
+  queryParams.push(property.parking_spaces)
+  queryParams.push(property.number_of_bathrooms)
+  queryParams.push(property.number_of_bedrooms)
+
+  let queryString = 
+  `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`
+  
+
+
+  return pool
+  .query(queryString, queryParams)
+  .then((result) => {
+    console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+    return null;
+  });
+  
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
